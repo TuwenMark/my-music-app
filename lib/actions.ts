dcde9207e815993d16b9cbb0425405b780b2ae99
MarkTuwen <m18698577021@163.com>
2025-04-18 06:53:40 +0800

    fix issues

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,6 +1,6 @@
 import { refreshCookie } from './neteasecloud/loginActions';
 
 export const refreshAndExecute = <T>(action: () => Promise<T>): Promise<T> => {
-  refreshCookie;
+  refreshCookie();
   return action();
 };
